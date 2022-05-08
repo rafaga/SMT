dd885e03e0df720210e8a19c2040416918834b9e
Rafael Amador Galván <rafael.amador@gmail.com>
2022-05-07 19:58:32 -0500

    new Adjustments an d testing on MacOS

--- SMTx/Views/NewVersionWindow.axaml.cs
+++ SMTx/Views/NewVersionWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
 using SMTx.ViewModels;
+using Avalonia.Platform;
 
 namespace SMTx.Views
 {
@@ -14,6 +15,11 @@ namespace SMTx.Views
 #if DEBUG
             this.AttachDevTools();
 #endif
+            OperatingSystemType oOS = AvaloniaLocator.Current.GetService<IRuntimePlatform>().GetRuntimeInfo().OperatingSystem;
+            if (oOS == OperatingSystemType.OSX || oOS == OperatingSystemType.Linux)
+            {
+                this.FindControl<Menu>("padRect").IsVisible = true;
+            }
         }
 
         private void InitializeComponent()
