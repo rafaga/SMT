0a9f3bcaa236b547eb09f27c82cbb8753ffc4d3f
Rafael Amador Galván <rafael.amador@gmail.com>
2022-05-26 21:43:33 -0500

    Basic ESI data retrieve is working (ESI status) and it is reflected on. SMTx GUI

---
+++ EVEData/ESIData/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace EVEData.ESIData
+{
+	/// <summary>
+    /// 
+    /// </summary>
+	public class Character
+	{
+		public long id { get; set; }
+		public string? name { get; set; }
+	}
+
+	/// <summary>
+    /// 
+    /// </summary>
+	public partial class CharacterIdData
+	{
+		public Character[]? Characters { get; set; }
+	}
+}
+
