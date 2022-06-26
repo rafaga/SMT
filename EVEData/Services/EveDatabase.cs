4e38d420e870e607f9af34534141df40eac3fe31
Rafael Amador Galván <rafael.amador@gmail.com>
2022-06-26 17:33:54 -0500

    Add a simple SQLite interface for use in the next version of SMT. Ending further development here and continuing in avalonia branch

---
+++ EVEData/Services/EveDatabase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace EVEData.Services
+{
+	public enum eDatabaseType
+    {
+		None,
+		SQLlite
+    }
+
+	public class EveDatabase
+	{
+
+		private IDbConnection? _connection;
+		private eDatabaseType? _dbType;
+
+		public eDatabaseType? DatabaseType
+        {
+			get => _dbType;
+			private set {
+				_dbType = value;
+            }
+        }
+
+		public EveDatabase(eDatabaseType type)
+		{
+			if(type == eDatabaseType.SQLlite)
+            {
+				_connection = new SqliteConnection();
+				_connection.ConnectionString = "Data Source=smt.db";
+            }
+			DatabaseType = type;
+		}
+
+		public IDbConnection? Connection
+        {
+			get => _connection;
+			private set
+            {
+				_connection = value;
+            }
+        }
+	}
+}
+
